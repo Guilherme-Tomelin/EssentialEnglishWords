f4cfa195850079c959d82d24ccba2600094f1803
Guilherme-Tomelin <guilhermesantosgts@outlook.com>
2023-08-09 14:31:24 -0300

    pull

--- words_api/urls.py
+++ words_api/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
-from words_api.views import PalavrasViewSet, JSONUploadView
+from words_api.views import PalavrasViewSet
 
 router = routers.DefaultRouter()
 router.register('words', PalavrasViewSet, basename='Words')
 
 urlpatterns = [
     path('api',include(router.urls)),
-    path('upload-json/', JSONUploadView.as_view(), name='json-upload'),
+    #path('upload-json/', JSONUploadView.as_view(), name='json-upload'),
 ]
--- words_api/views.py
+++ words_api/views.py
@@ -12,21 +12,21 @@ class PalavrasViewSet(viewsets.ModelViewSet):
 
 
 # views.py
-from rest_framework.parsers import JSONParser
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework import status
-from .models import Palavra
-from .serializer import WordSerializer
+# from rest_framework.parsers import JSONParser
+# from rest_framework.response import Response
+# from rest_framework.views import APIView
+# from rest_framework import status
+# from .models import Palavra
+# from .serializer import WordSerializer
 
-class JSONUploadView(APIView):
-    parser_classes = [JSONParser]
+# class JSONUploadView(APIView):
+#     parser_classes = [JSONParser]
 
-    def post(self, request, format=None):
-        data = request.data.get('data', [])  # Obtém a lista de palavras do JSON
-        serializer = WordSerializer(data=data, many=True)
+#     def post(self, request, format=None):
+#         data = request.data.get('data', [])  # Obtém a lista de palavras do JSON
+#         serializer = WordSerializer(data=data, many=True)
 
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
