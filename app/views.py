cbf572216dbfaf7a6ed3a6c2705e1c7b1d4660be
Guilherme-Tomelin <guilhermesantosgts@outlook.com>
2023-08-09 14:31:24 -0300

    pull

--- app/views.py
+++ app/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     return render(request, 'app/index.html')
 
 def conteudo(request):
+    if not request.user.is_authenticated:
+        return redirect('/login')    
+        
     return render(request, 'app/conteudo.html')
 
 
